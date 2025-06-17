a510926593a2b7431ab466b5e0dfa824e538fee8
maelsilvatt <ismaelsoares.pro@gmail.com>
2025-06-16 23:30:59 -0300

    Refactor BuriedBornesEnv: replace random position generation with np_random, update reset and step methods to align with gymnasium API

--- envs/buried_bornes_env.py
+++ envs/buried_bornes_env.py
@@ -1,7 +1,6 @@
 import gymnasium as gym
 import numpy as np
 from gymnasium import spaces 
-import random
 
 class BuriedBornesEnv(gym.Env):
     """
@@ -16,33 +15,32 @@ class BuriedBornesEnv(gym.Env):
         self.enemy_damage = 2
         self.agent_damage = 3
 
-        # Define action space: 0=up, 1=down, 2=left, 3=right, 4=attack
         self.action_space = spaces.Discrete(5)
 
-        # Observation space: [agent_x, agent_y, agent_hp, enemy_x, enemy_y, enemy_hp]
         self.observation_space = spaces.Box(
             low=0,
             high=max(self.grid_size, self.max_hp),
             shape=(6,),
             dtype=np.int32
-        )
+        )    
 
-        self.reset()
+    def reset(self, seed=None, options=None):        
+        super().reset(seed=seed)
 
-    def reset(self):
         self.agent_pos = [0, 0]
         self.agent_hp = self.max_hp
 
-        self.enemy_pos = [random.randint(0, self.grid_size - 1), random.randint(0, self.grid_size - 1)]
+        self.enemy_pos = self.np_random.integers(0, self.grid_size, size=2).tolist()
         while self.enemy_pos == self.agent_pos:
-            self.enemy_pos = [random.randint(0, self.grid_size - 1), random.randint(0, self.grid_size - 1)]
+            self.enemy_pos = self.np_random.integers(0, self.grid_size, size=2).tolist()
         self.enemy_hp = self.max_hp
-
-        return self._get_obs()
+        
+        return self._get_obs(), {}
 
     def step(self, action):
-        reward = -0.1  # slight penalty to encourage efficient play
-        done = False
+        reward = -0.1        
+        terminated = False 
+        truncated = False  
 
         # Movement actions
         if action == 0 and self.agent_pos[1] > 0:
@@ -53,13 +51,12 @@ class BuriedBornesEnv(gym.Env):
             self.agent_pos[0] -= 1
         elif action == 3 and self.agent_pos[0] < self.grid_size - 1:
             self.agent_pos[0] += 1
-
         # Attack action
         elif action == 4:
             if self.agent_pos == self.enemy_pos and self.enemy_hp > 0:
                 self.enemy_hp -= self.agent_damage
                 reward += 1
-
+        
         # Enemy turn if still alive
         if self.enemy_hp > 0:
             if self.agent_pos == self.enemy_pos:
@@ -69,12 +66,12 @@ class BuriedBornesEnv(gym.Env):
         # Check end conditions
         if self.enemy_hp <= 0:
             reward += 5
-            done = True
+            terminated = True
         if self.agent_hp <= 0:
             reward -= 5
-            done = True
-
-        return self._get_obs(), reward, done, {}
+            terminated = True
+        
+        return self._get_obs(), reward, terminated, truncated, {}
 
     def _get_obs(self):
         return np.array([
